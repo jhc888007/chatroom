4599082f652aedbfb39dad3aca45439f5b34fc47
jhc888007 <jhc888007@163.com>
2017-05-13 09:18:38 -0700

    4

--- server/server.cpp
+++ server/server.cpp
@@ -14,6 +14,7 @@
 
 #define MAXLINE 1024
 #define MAXCONN 1024
+#define MAXTIMEMINUTE 2
 
 void *server_thread(void *arg) {
     int connect_socket_id = *(int*)arg;
@@ -58,10 +59,55 @@ int main(int argc,char **argv) {
     }
     MLOG(INFO, "Server Listening");
 
-    int connect_socket_id;
-    std::vector<pthread_t> thread_vector;
+    int connect_socket_id, max_socket_id;
+    //std::vector<pthread_t> thread_vector;
+    fd_set fdsr;
+    int fd[MAXCONN];
+    time_t time_fd[MAXCONN];
+    time_t time_last, time_now;
+    bool time_change;
     for (;;) {
-        if ((connect_socket_id = accept(listen_socket_id, (struct sockaddr*)NULL, NULL))==-1) {
+        time_change = false;
+        time_now = time((time_t*)NULL);
+        if (time_last - time_now >= 60) {
+            time_change = true;
+            time_last = time_now;
+        }
+
+        FD_ZERO(&fdsr);
+        FD_SET(listen_socket_id, &fdsr);
+        max_socket_id = listen_socket_id;
+        for (int i = 0; i < MAXCONN; i++) {
+            if (fd[i]) {
+                FD_SET(fd[i], &fdsr);
+                if (time_change) {
+                    time_fd[i]--;
+                    if (time_fd[i] =< 0) {
+                        close(fd[i]);
+                        FD_CLR(fd[i], &fdsr);
+                        fd[i] = 0;
+                    }
+                }
+                if (fd[i] && fd[i] > max_socket_id) {
+                    max_socket_id = fd[i];
+                }
+            }
+        }
+
+        struct timeval timeout;
+        timeout.tv_sec = 1;
+        timeout.tv_usec = 0;
+        int ret = select(max_socket_id+1, &fdsr, NULL, NULL, &timeout);
+
+        if (ret < 0) {
+            MLOG(ERROR, "Select Error");
+            break;
+        } else if (ret == 0) {
+            MLOG(INFO, "Select Timeout");
+            continue;
+        }
+
+        /*if ((connect_socket_id = accept(listen_socket_id, (struct sockaddr*)NULL, NULL))==-1) {
             MLOG(ERROR, "Server Accpet Error: %s Errno: %d", strerror(errno), errno);
             continue;
         }
@@ -73,8 +119,14 @@ int main(int argc,char **argv) {
         }
         thread_vector.push_back(thread_id);
         MLOG(INFO, "Thread Create Success, Thread: %d", (int)thread_id);
+        */
 
-        /*pid_t pid = fork();
+        /*if ((connect_socket_id = accept(listen_socket_id, (struct sockaddr*)NULL, NULL))==-1) {
+            MLOG(ERROR, "Server Accpet Error: %s Errno: %d", strerror(errno), errno);
+            continue;
+        }
+        
+        pid_t pid = fork();
         if (pid == -1) {
             MLOG(ERROR, "Server Fork Fail");
         }
@@ -94,7 +146,6 @@ int main(int argc,char **argv) {
             MLOG(INFO, "Server New Fork Pid: %d", (int)pid);
             close(connect_socket_id);
         }*/
-        //shutdown(connect_socket_id, SHUT_RDWR);
     }
 
     for (std::vector<pthread_t>::iterator it = thread_vector.begin(); it != thread_vector.end(); it++) {
