d246bf44917ef6a6ca8c480b4fc40eaa4bc05a8d
jhc888007 <jhc888007@163.com>
2017-06-05 22:50:23 +0800

    12

--- server/server.cpp
+++ server/server.cpp
@@ -14,6 +14,9 @@
 #include <sys/epoll.h>
 #include <set>
 #include <map>
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
 
 #include "log/log.hpp"
 #include "proto/net_message.pb.h"
@@ -53,85 +56,167 @@ public:
     }
 };
 
+enum {
+    EPOLL_EVENT_OUT,
+    EPOLL_EVENT_IN,
+    EPOLL_EVENT_ERROR,
+    EPOLL_EVENT_INVALID_INPUT,
+};
+
+
+class Epoll {
+    enum {
+        MAX_EVENT_NUM = 512,
+        MAX_CONNECT_NUM = 1024,
+    };
+    typedef std::set<int> FdSet;
+    FdSet _fd_set;
+    int _epoll_fd;
+
+    int _epoll_event_num;
+    struct epoll_event _epoll_event_out[MAX_EVENT_NUM];
+public:
+    Epoll(int max_connect_num = MAX_CONNECT_NUM) {
+        _epoll_fd = epoll_create(max_connect_num<MAX_CONNECT_NUM?max_connect_num:MAX_CONNECT_NUM);
+    };
+    ~Epoll() {
+        close(_epoll_fd);
+    }
+
+    void AddEvent(int fd) {
+        struct epoll_event tmp;
+        tmp.events = EPOLLIN|EPOLLET;
+        tmp.data.fd = fd;
+        int ret;
+        if (_fd_set.find(fd) != _fd_set.end()) {
+            ret = epoll_ctl(_epoll_fd, EPOLL_CTL_MOD, fd, &tmp);
+        } else {
+            ret = epoll_ctl(_epoll_fd, EPOLL_CTL_ADD, fd, &tmp);
+            if (ret >= 0) {
+                _fd_set.insert(fd);
+            }
+        }
+        if (ret) {
+            MLOG(ERROR, "Epoll Add Event Error, Fd: %d", fd);
+        } else {
+            MLOG(INFO, "Epoll Add Event Success, Fd: %d", fd);
+        }
+        return;
+    }
+    void DelEvent(int fd) {
+        if (_fd_set.find(fd) == _fd_set.end()){
+            MLOG(ERROR, "Epoll Del Event Error, Fd: %d", fd);
+            return;
+        }
+        struct epoll_event tmp;
+        tmp.data.fd = fd;
+        epoll_ctl(_epoll_fd, EPOLL_CTL_DEL, fd, &tmp);
+        return;
+    }
+    inline int Wait(int timeout = 1000) {
+        _epoll_event_num = epoll_wait(_epoll_fd, _epoll_event_out, MAX_EVENT_NUM, timeout);
+        return _epoll_event_num;
+    }
+    inline int GetEvent(int i, int &fd) {
+        if (i >= _epoll_event_num) return EPOLL_EVENT_INVALID_INPUT;
+        unsigned int event = _epoll_event_out[i].events;
+        fd = _epoll_event_out[i].data.fd;
+        if ((event & EPOLLERR) || (event & EPOLLHUP)) return EPOLL_EVENT_ERROR;
+        if (event & POLLIN) return EPOLL_EVENT_IN;
+        if (event & POLLOUT) return EPOLL_EVENT_OUT;
+        return true;
+    }
+};
+
+class Shared {
+    sem_t *_sem;
+    char *_buffer;
+    int _size;
+    int _cap;
+    bool _is_valid;
+public:
+    Shared(int len = 4000) {
+        _is_valid = false;
+        len = ((len+sizeof(sem_t)/4096)+1)*4096;
+        _buffer = (char *)mmap(0, len, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANON, -1, 0);
+        if (_buffer == (char *)-1) {
+            MLOG(ERROR, "Shared Memory Error");
+            return;
+        }
+        _size = 0;
+        _cap = len - sizeof(sem_t);
+        _sem = (sem_t *)(_buffer + _cap);
+        int ret = sem_init(_sem, 1, 1);
+        if (ret != 0) {
+            MLOG(ERROR, "Sem Init Error");
+            return;
+        }
+        _is_valid = true;
+    }
+    ~Shared() {
+        if (IsValid())
+            sem_destroy(_sem);
+    }
+    inline bool IsValid() const {return _is_valid == true;}
+    inline bool IsEmpty() const {return _size == 0;}
+    inline const int getCap() const {return _cap;}
+    inline const int getSize() const {return _size;}
+    void Lock() {
+        if (IsValid())
+            sem_wait(_sem);
+    }
+    void Unlock() {
+        if (IsValid())
+            sem_post(_sem);
+    }
+    bool Read(char *buffer, int max, int *len) {
+        if (_size == 0 || !IsValid()) {
+            *len = 0;
+            MLOG(INFO, "AAA:%d", _size);
+            return false;
+        }
+        Lock();
+        max = _size < max ? _size : max;
+        memcpy(buffer, _buffer, max);
+        _size = 0;
+        Unlock();
+        return true;
+    }
+    bool Write(char *buffer, int len) {
+        if (_cap == 0 || !IsValid()) {
+            MLOG(ERROR, "Share Memory Write Error");
+            return false;
+        }
+        Lock();
+        len = len < _cap ? len : _cap;
+        memcpy(_buffer, buffer, len);
+        _size = len;
+        Unlock();
+        return true;
+    }
+    bool Append(char *buffer, int len) {
+        if (_cap == 0 || !IsValid()) {
+            MLOG(ERROR, "Share Memory Append Error");
+            return false;
+        }
+        Lock();
+        len = len < _cap - _size ? len : _cap - _size;
+        memcpy(_buffer, buffer, len);
+        _size += len;
+        MLOG(INFO, "ABB:%d", _size);
+        Unlock();
+        return true;
+    }
+};
+
 class Poll {
 public:
-    enum {
-        EVENT_OUT,
-        EVENT_IN,
-        EVENT_ERROR,
-        EVENT_INVALID_INPUT,
-    };
     enum {
         POLL_SUCCESS = 0,
         POLL_ERROR,
     };
 
-    class Epoll {
-        enum {
-            MAX_EVENT_NUM = 512,
-            MAX_CONNECT_NUM = 1024,
-        };
-        typedef std::set<int> FdSet;
-        FdSet fd_set;
-        int epoll_fd;
-
-        int epoll_event_num;
-        struct epoll_event epoll_event_out[MAX_EVENT_NUM];
-    public:
-        Epoll(int listen_socket_fd, int max_connect_num = MAX_CONNECT_NUM) {
-            epoll_fd = epoll_create(max_connect_num<MAX_CONNECT_NUM?max_connect_num:MAX_CONNECT_NUM);
-            AddEvent(listen_socket_fd, EPOLLIN|EPOLLET);
-        };
-        ~Epoll() {
-            close(epoll_fd);
-        }
-
-        void AddEvent(int fd, int events) {
-            struct epoll_event tmp;
-            tmp.events = events;
-            tmp.data.fd = fd;
-            int ret;
-            if (fd_set.find(fd) != fd_set.end()) {
-                ret = epoll_ctl(epoll_fd, EPOLL_CTL_MOD, fd, &tmp);
-            } else {
-                ret = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &tmp);
-                if (ret >= 0) {
-                    fd_set.insert(fd);
-                }
-            }
-            if (ret) {
-                MLOG(ERROR, "Epoll Add Event Error, Fd: %d", fd);
-            } else {
-                MLOG(INFO, "Epoll Add Event Success, Fd: %d", fd);
-            }
-            return;
-        }
-        void DelEvent(int fd) {
-            if (fd_set.find(fd) == fd_set.end()){
-                MLOG(ERROR, "Epoll Del Event Error, Fd: %d", fd);
-                return;
-            }
-            struct epoll_event tmp;
-            tmp.data.fd = fd;
-            epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, &tmp);
-            return;
-        }
-        inline int Wait(int timeout = 1000) {
-            epoll_event_num = epoll_wait(epoll_fd, epoll_event_out, MAX_EVENT_NUM, timeout);
-            return epoll_event_num;
-        }
-        inline int GetEvent(int i, int &fd) {
-            if (i >= epoll_event_num) return Poll::EVENT_INVALID_INPUT;
-            unsigned int event = epoll_event_out[i].events;
-            fd = epoll_event_out[i].data.fd;
-            if ((event & EPOLLERR) || (event & EPOLLHUP)) return Poll::EVENT_ERROR;
-            if (event & POLLIN) return Poll::EVENT_IN;
-            if (event & POLLOUT) return Poll::EVENT_OUT;
-            return true;
-        }
-    };
-         
-private:
+//private:
     struct SocketInfo {
         int _fd;
         struct sockaddr_in _addr;
@@ -142,69 +227,92 @@ private:
         }
     };
     typedef std::map<int, struct SocketInfo> SocketMap;
-    SocketMap socket_map;
-    int listen_fd;
-    struct sockaddr_in listen_addr;        
-    Epoll *p_epoll;
+    SocketMap _socket_map;
+    int _listen_fd;
+    struct sockaddr_in _listen_addr;        
+    Epoll *_p_epoll;
+    Shared *_p_shared;
 
-    void addSocketMap(int fd, const struct sockaddr_in &addr) {
-        struct SocketInfo *info = new SocketInfo(fd, addr);
-        socket_map.insert(std::make_pair(fd, *info));
+    void addSocketMap(int fd, const struct sockaddr_in *addr) {
+        struct SocketInfo *info = new SocketInfo(fd, *addr);
+        _socket_map.insert(std::make_pair(fd, *info));
     }
     void delSocketMap(int fd) {
-        SocketMap::iterator it = socket_map.find(fd);
-        if (it != socket_map.end())
-            socket_map.erase(it);
+        SocketMap::iterator it = _socket_map.find(fd);
+        if (it != _socket_map.end())
+            _socket_map.erase(it);
     }
     Octets *getReadBuffer(int fd) {
-        SocketMap::iterator it = socket_map.find(fd);
-        if (it != socket_map.end())
+        SocketMap::iterator it = _socket_map.find(fd);
+        if (it != _socket_map.end())
             return &(it->second._rOs);
         return NULL;
     }
     Octets *getWriteBuffer(int fd) {
-        SocketMap::iterator it = socket_map.find(fd);
-        if (it != socket_map.end())
+        SocketMap::iterator it = _socket_map.find(fd);
+        if (it != _socket_map.end())
             return &(it->second._wOs);
         return NULL;
     }
- 
-public:
-    Poll(int port_num) {
-        if ((listen_fd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
-            listen_fd = 0;
-            MLOG(ERROR, "Server Socket Error, %d", listen_fd);
-            return ;
-        }
-        MLOG(INFO, "Server Socket Open, %d", listen_fd);
+    void addNewSocketFather(int fd) {
+        _p_shared->Append((char *)&fd, sizeof(fd));
+    }
 
-        memset(&listen_addr,0,sizeof(listen_addr));
-        listen_addr.sin_family = AF_INET;
-        listen_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-        listen_addr.sin_port = htons(port_num);
-        p_epoll = (Epoll *)new Epoll(listen_fd);
+public:
+    Poll() {
+        _listen_fd = 0;
+        memset(&_listen_addr,0,sizeof(_listen_addr));
+        _p_epoll = (Epoll *)new Epoll();
+        _p_shared = (Shared *)new Shared();
     }
     ~Poll() {
-        delete p_epoll;
-        for (SocketMap::iterator it = socket_map.begin(); it != socket_map.end(); it++) {
+        delete _p_epoll;
+        delete _p_shared;
+        for (SocketMap::iterator it = _socket_map.begin(); it != _socket_map.end(); it++) {
             MLOG(INFO, "Connect Socket Close, %d", it->first);
             close(it->first);
         }
-        MLOG(INFO, "Server Socket Close, %d", listen_fd);
-        close(listen_fd);
+        MLOG(INFO, "Server Socket Close, %d", _listen_fd);
+        if (IsListenFdValid())
+            close(_listen_fd);
     }
-    inline bool IsValid() {return listen_fd==0;}
+    inline int InitListen(int port_num) {
+        if (_p_epoll == NULL)
+            return POLL_ERROR;
+
+        if ((_listen_fd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
+            _listen_fd = 0;
+            MLOG(ERROR, "Server Socket Error, %d", _listen_fd);
+            return POLL_ERROR;
+        }
+        MLOG(INFO, "Server Socket Open, %d", _listen_fd);
+
+        _listen_addr.sin_family = AF_INET;
+        _listen_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        _listen_addr.sin_port = htons(port_num);
+        
+        int opt = 1;
+        if (setsockopt(_listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+            MLOG(ERROR, "Set Socket Error, %d", _listen_fd);
+            return POLL_ERROR;
+        }
+
+        _p_epoll->AddEvent(_listen_fd);
+        return POLL_SUCCESS;
+    }
+    inline bool IsListenFdValid() {return _listen_fd!=0;}
+    inline bool IsEpollValid() {return _p_epoll!=NULL;}
     inline int BindAndListen() {
         int ret;
-        if ((ret = bind(listen_fd, (struct sockaddr *)&listen_addr, sizeof(listen_addr))) < 0) {
-            MLOG(ERROR, "Server Bind Error, %d, %x", ntohs(listen_addr.sin_port),
-                ntohl(listen_addr.sin_addr.s_addr));
+        if ((ret = bind(_listen_fd, (struct sockaddr *)&_listen_addr, sizeof(_listen_addr))) < 0) {
+            MLOG(ERROR, "Server Bind Error, %d, %x", ntohs(_listen_addr.sin_port),
+                ntohl(_listen_addr.sin_addr.s_addr));
             return ret;
         }
-        MLOG(INFO, "Server Bind, %d, %x", ntohs(listen_addr.sin_port),
-            ntohl(listen_addr.sin_addr.s_addr));
+        MLOG(INFO, "Server Bind, %d, %x", ntohs(_listen_addr.sin_port),
+            ntohl(_listen_addr.sin_addr.s_addr));
         
-        if ((ret = listen(listen_fd, MAXCONN)) < 0) {
+        if ((ret = listen(_listen_fd, MAXCONN)) < 0) {
             MLOG(ERROR, "Server Listen Error");
             return ret;
         }
@@ -212,8 +320,21 @@ public:
         MLOG(INFO, "Server Listening");
         return 0;
     }
+    void SyncNewSocketChild() {
+        char buf[100];
+        int len;
+        _p_shared->Read(buf, 100, &len);
+        if (len > 0) {
+            int *fds = (int *)&buf[0];
+            len /= sizeof(int);
+            for (int i = 0; i < len; i++) {
+                MLOG(INFO, "Child Sync Fd: %d", fds[i]);
+                _p_epoll->AddEvent(fds[i]);
+            }
+        }
+    }
     inline int Execute() {
-        int event_num = p_epoll->Wait();
+        int event_num = _p_epoll->Wait();
 
         if (event_num < 0) {
             MLOG(ERROR, "Poll Wait Error");
@@ -224,48 +345,53 @@ public:
 
         for (int i = 0; i < event_num; i++) {
             int fd;
-            int ret = p_epoll->GetEvent(i, fd);
+            int ret = _p_epoll->GetEvent(i, fd);
 
-            if (fd == listen_fd) {
+            if (IsListenFdValid() && fd == _listen_fd) {
                 int connect_fd;
-                struct sockaddr_in connect_addr;        
+                struct sockaddr connect_addr;        
                 unsigned int len;
-                connect_fd = accept(listen_fd, (struct sockaddr *)&listen_addr, (socklen_t *)&len);
+                connect_fd = accept(_listen_fd, (struct sockaddr *)&connect_addr, (socklen_t *)&len);
                 if (connect_fd < 0) {
                     MLOG(ERROR, "Accept Error");
-                } else if (len != sizeof(connect_addr)) {
+                } else if (len != sizeof(sockaddr_in)) {
                     MLOG(ERROR, "Accept Struct Size Unknown");
                     close(connect_fd);
+                    delSocketMap(connect_fd);
                 } else {
-                    p_epoll->AddEvent(connect_fd, EPOLLIN|EPOLLET);
-                    addSocketMap(connect_fd, connect_addr);
+                    MLOG(INFO, "Accept New Connection: %d", connect_fd);
+                    addNewSocketFather(connect_fd);
+                    _p_epoll->AddEvent(connect_fd);
+                    addSocketMap(connect_fd, (struct sockaddr_in *)&connect_addr);
                 }
                 continue;
             }
 
             Octets *os;
             switch(ret) {
-            case EVENT_INVALID_INPUT:
+            case EPOLL_EVENT_INVALID_INPUT:
                 MLOG(ERROR, "Get Event Invalid Input");
                 return POLL_ERROR;
-            case EVENT_ERROR:
-                MLOG(INFO, "Get Event Socket Poll Error");
-                p_epoll->DelEvent(fd);
+            case EPOLL_EVENT_ERROR:
+                MLOG(INFO, "Get Event Socket Poll Error, %d", fd);
+                _p_epoll->DelEvent(fd);
                 close(fd);
+                delSocketMap(fd);
                 break;
-            case EVENT_IN:
+            case EPOLL_EVENT_IN:
                 os = getReadBuffer(fd);
                 int result;
                 result = recv(fd, os->GetCurrent(), os->GetBlank(), 0);
                 if (ret <= 0) {
                     MLOG(INFO, "Client Recv Error Fd Close, %d", fd);
-                    p_epoll->DelEvent(fd);
+                    _p_epoll->DelEvent(fd);
                     close(fd);
+                    delSocketMap(fd);
                 } else {
                     MLOG(INFO, "Client Receive, Fd: %d, Len: %d", fd, result);
                 }
                 break;
-            case EVENT_OUT:
+            case EPOLL_EVENT_OUT:
                 os = getWriteBuffer(fd);
                 MLOG(INFO, "Client Send, Fd: %d", fd);
                 break;
@@ -279,18 +405,45 @@ public:
 };
 
 int main(int argc,char **argv) {
-    Poll *poll = new Poll(10004);
-    int ret = poll->BindAndListen();
-    if (ret < 0) {
-        MLOG(ERROR, "Bind And Listen: %d", ret);
+    Poll *poll = new Poll();
+    int ret;
+
+    pid_t pid = fork();
+    if (pid < 0) {
+        MLOG(ERROR, "Fork Error: %d", ret);
         exit(0);
-    } 
+    }
+
+    if (pid > 0) {
+        MLOG(INFO, "Father1");
+        ret = poll->InitListen(10004);
+        if (ret < 0) {
+            MLOG(ERROR, "Listen Init Error: %d", ret);
+            exit(0);
+        }
+
+        ret = poll->BindAndListen();
+        if (ret < 0) {
+            MLOG(ERROR, "Bind And Listen: %d", ret);
+            exit(0);
+        } 
+    }
+
+    bool new_flag;
+    int new_connection;
     while (1) {
+        if (pid == 0) {
+            poll->SyncNewSocketChild();
+            sleep(2);
+            continue ;
+        }
+
         ret = poll->Execute();
         if (ret) {
             MLOG(ERROR, "Poll Error: %d", ret);
             break;
         }
     }
+
     return 0;
 }
